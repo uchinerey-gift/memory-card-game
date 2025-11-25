22cab3810039d2ae49ee6582fc3902ebbcf4e6c6
Chinerey Ukwu <uchinerey@gmail.com>
2025-11-25 01:13:15 -0700

    Add project overview paragraph describing game plan

---
+++ script.js
@@ -0,0 +1,8 @@
+// Project Overview:
+// For this project I am building a simple Memory Card Game using HTML and JavaScript. 
+// The goal is to flip two cards at a time and try to find all the matching pairs. 
+// I will create an array of card values, shuffle them, and display them on the page as clickable cards. 
+// When the player clicks two cards, I will check if they match and either keep them flipped or flip them back. 
+// I will also track the number of moves and use a timer to show how long the game takes. 
+// My JavaScript will include functions for setting up the game, handling clicks, checking matches,
+// updating the moves, and restarting the game.
